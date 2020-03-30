a72242a6d8c96b121f8095e02320d855b7cafa0b
Jihoon Park <bartleby2718@gmail.com>
2020-03-29 21:53:48 -0400

    Use Bootstrap to do minimum styling

--- quotes/forms.py
+++ quotes/forms.py
@@ -9,6 +9,12 @@ class SendTextForm(forms.Form):
         message='Phone number should be exactly 10 digits.'
     )
     number = forms.CharField(
-        min_length=10, max_length=10,
+        min_length=10, max_length=10, label='Your phone number',
+        widget=forms.TextInput(attrs={
+            'class': 'form-group mx-sm-3 mb-2',
+            'style': 'width: 250px',
+            'aria-describedby': 'help',
+            'placeholder': 'Enter your 10-digit phone number',
+        }),
         validators=[number_validator]
     )
