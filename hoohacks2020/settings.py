f3aea9aa98f4f9cff9f8f4ad1222ea548c6e9e3e
Jihoon Park <bartleby2718@gmail.com>
2020-03-29 22:05:43 -0400

    Set DEBUG = False

--- hoohacks2020/settings.py
+++ hoohacks2020/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#z8vgu_*$0+we=fewddpkps7_g(tlkf(&g93h4ka$#hefj&(gb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
